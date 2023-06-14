15f6ba274bfef5928118b5cf63646280b749f7d4
kenric <kenricqq@gmail.com>
2023-06-14 07:28:20 -0700

    fixed last draw bug

---
+++ features.md
@@ -0,0 +1,41 @@
+*Always have plenty inspiration from Khan Academy and 3Blue1Brown.(smiley face)*
+
+Before I go on, I should think about why this app would even be useful for anyone, other than the fact that it's cool and random. To have a selling point, I must connect this product and how it's going to be used to how it'll benefit other people.
+
+Math is numbers, logic, and often cause the seemingly imperfect perfect. Like how pi, which is a irrational number, be represented by perfectly simple integers. The infinite series, patterns, and shapes that result with math formulas, fibonnaci, etc... I think much of the world and its population lack the ability to explore the beauty of math. One selling point of this app would be that it allows artists, now easier than ever, incorporate the beauty of math into art. Art could have intricate patterns that follow math logic, and as such become more "*perfect*" with a smooth "*end*". Like a perfectly round circle, rather than a close-to-perfect hand-drawn circle. Of course, it'll have to be more than circle, computers nowadays have the capabilities to do that and more already. The selling point would be more geared towards derivatives and other slopes and curves that are less often regarded as math. The random zigzags and curves that are ultimately a series of expressions combined. Although curves and other mathematical technology exists, what doesn't yet is the bridge between user and creator. Most people have no idea what to make, as they lack the experience of working with shapes, and as a result, are given a tool that is merely serving those who have a clear plan in mind, while leaving those searching for inspiration blank and clueless. This plans to simplify that a bit with user exploration phases and allowing to properly understand and later on craft their work of art.
+
+## Polynomial
+draw polynomial graphs with lines, like yesterday's string art, and possibly find a way to incorporate color
+
+## Color
+user enters the rgb, or set mouseX() or mouseY() to any of the values
+
+## shapes (solved)
+now equilateral shapes are available given number of sides
+
+## tapDraw
+need to be able to create shapes with a button click, not needing mousemove...
+
+## menu for shapes selection
+need to add currentShape variable, and have fields for user to enter based on chosen shape (polygon, rect, circle)
+
+### buttons and sliders
+- to change border thickness
+- to add box-shadow
+- 
+
+## rotate shapes
+be able to interact with ui, having a snap in place rotating mechanism that allows for 15 degrees each
+
+## automated mouse movement
+automated mouse movement for desired geometry patterns; how much to increment each mouse position by
+
+## undo and clear button deprecated
+
+
+app is revolved around the use of **circles** and **geometry**
+
+
+# advanced features
+
+### make 2d appear like 3d, without the need of having access to 3d, like using perspectives when drawing, maybe based on AI?
---
+++ polygon.js
@@ -0,0 +1,190 @@
+// defaults
+let canvas = document.getElementById('canvas')
+canvas.width = window.innerWidth - 60
+canvas.height = window.innerHeight - 400
+let numSides = 5
+let global_radius = 50
+let drawDelay = 2
+
+// canvas setup
+let ctx = canvas.getContext('2d')
+let canvasColor = '#f7eee9'
+ctx.fillStyle = canvasColor
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+let rotateBy = 0 // range: [0, 360), convert to radians
+let drawColor = 'black'
+let isDrawing = false
+let restoreArray = []
+let index = -1
+
+// title
+const canvasType = document.getElementById('canvasType')
+canvasType.innerHTML = `Polygon`
+
+// change color
+function change_color(element) {
+  drawColor = element.style.background
+}
+
+// mouse events
+canvas.addEventListener('mousedown', tapDraw, false)
+canvas.addEventListener('mousemove', drawCond, false)
+canvas.addEventListener('mouseup', stop, false)
+canvas.addEventListener('mouseout', stop, false)
+
+// touch events
+canvas.addEventListener('touchstart', start, false)
+canvas.addEventListener('touchend', stop, false)
+
+// tap to draw single instance
+function tapDraw() {
+  ctx.strokeStyle = drawColor
+  start()
+  drawPolygon(numSides, global_radius, -Math.PI / 2)
+}
+
+// delay to add spacing between polygons
+let count = -1
+function drawCond() {
+  // draw()
+  if (isDrawing) {
+    console.log(drawDelay)
+    if (count == 0) draw()
+    else if (count == drawDelay) count = -1
+    count++
+  }
+}
+
+// initialize image in array
+function start() {
+  isDrawing = true
+  ctx.beginPath()
+}
+
+function draw() {
+  if (isDrawing) {
+    ctx.strokeStyle = drawColor
+    drawPolygon(numSides, global_radius, -Math.PI / 2)
+    // drawRect(x(), y());
+    // drawEllipse(x(), y())
+    console.log(x(), y())
+    // console.log(canvas.height, canvas.width)
+    // ctx.fillStyle = `rgb(100,${y()},${x()})`;
+    // ctx.fill();
+  }
+}
+
+// function drawEllipse(hr, vr) {
+//   ctx.ellipse(x(), y(), hr, vr, Math.PI, 0, 2 * Math.PI)
+//   ctx.stroke()
+// }
+
+function drawPolygon(sides, radius, rotateBy) {
+  console.log(sides)
+  let full = 2 * Math.PI
+  dtheta = full / sides
+  ctx.moveTo(
+    x() + radius * Math.cos(rotateBy),
+    y() + radius * Math.sin(rotateBy)
+  )
+  for (let t = rotateBy + dtheta; t <= full + rotateBy; t += dtheta)
+    ctx.lineTo(x() + radius * Math.cos(t), y() + radius * Math.sin(t))
+  ctx.stroke()
+  ctx.closePath()
+  console.log('drawPolygon...')
+}
+
+// function drawRect(w, h) {
+//   ctx.rect(x() - w / 2, y() - h / 2, w, h)
+//   ctx.stroke()
+// }
+
+//closes path and disable drawing
+function stop() {
+  if (isDrawing) {
+    ctx.closePath()
+    isDrawing = false
+  }
+
+  if (event.type != 'mouseout') {
+    restoreArray.push(ctx.getImageData(0, 0, canvas.width, canvas.height))
+    index += 1
+  }
+  console.log(restoreArray)
+}
+
+function clearCanvas() {
+  ctx.fillStyle = canvasColor
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+  restoreArray = []
+  index = -1
+}
+
+function undo() {
+  if (index <= 0) {
+    clearCanvas()
+    console.log('undo')
+  } else {
+    index -= 1
+    restoreArray.pop()
+    ctx.putImageData(restoreArray[index], 0, 0)
+  }
+}
+
+// x and y coordinate
+function x() {
+  return event.clientX - canvas.offsetLeft
+  // due to canvas margin on left
+}
+function y() {
+  return event.clientY - canvas.offsetTop
+}
+
+// resize on window change
+window.addEventListener('resize', () => {
+  const canvasContainer = document.getElementById('canvasContainer')
+
+  const newCanvas = document.createElement('canvas')
+  const newContext = newCanvas.getContext('2d')
+  newCanvas.width = window.innerWidth - 60
+  newCanvas.height = 500
+  newContext.fillStyle = canvasColor
+  newContext.fillRect(0, 0, newCanvas.width, newCanvas.height)
+
+  newContext.drawImage(canvas, 0, 0)
+  canvasContainer.replaceChild(newCanvas, canvas)
+
+  canvas = newCanvas
+  ctx = newContext
+
+  canvas.addEventListener('mousedown', tapDraw, false)
+  canvas.addEventListener('mousemove', drawCond, false)
+  canvas.addEventListener('mouseup', stop, false)
+  canvas.addEventListener('mouseout', stop, false)
+})
+
+// copy canvas to clipboard
+function copToClip() {
+  canvas.toBlob((blob) => {
+    const item = new ClipboardItem({ 'image/png': blob })
+
+    navigator.clipboard
+      .write([item])
+      .then(() => {
+        console.log('Canvas image copied to clipboard!')
+      })
+      .catch((error) => {
+        console.error('Unable to copy canvas image to clipboard:', error)
+      })
+  }, 'image/png')
+}
+
+// change canvas color
+const canvasColorPicker = document.getElementById('canvasColorPicker')
+canvasColorPicker.addEventListener('change', () => {
+  canvasColor = canvasColorPicker.value
+  ctx.fillStyle = canvasColor
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+})
